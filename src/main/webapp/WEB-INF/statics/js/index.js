a1a5eb2238c76a6f2d117d71c4153f5512af3ba2
Aly0 <qianmei_li@163.com>
2017-06-27 17:20:20 +0800

    ll
    主体部分导航

--- src/main/webapp/WEB-INF/statics/js/index.js
+++ src/main/webapp/WEB-INF/statics/js/index.js
@@ -1,25 +1,28 @@
 //导航栏激活状态
 $(document).ready(function (){
-	$("li").each(function(index){
+	$("#navbar li").each(function(index){
 		$(this).mouseover(function(){ 
-			$("li").removeClass("active nav_active");
-			$("li").eq(index).addClass("active nav_active");
+			$("#navbar li").removeClass("active nav_active");
+			$("#navbar li").eq(index).addClass("active nav_active");
 		});
 	});
 	
-	//导航栏点击事件
+	//搜索框点击事件
 	$("#search").click(function(event){
 		$("#search").removeClass("col-md-3").addClass("col-md-7");
-		$("ul").hide();
+		$("#navbar ul").hide();
 		event.stopPropagation();
 	});
 	$(document).click(function(){
         $("#search").removeClass("col-md-7").addClass("col-md-3");
-        $("ul").show();
+        $("#navbar ul").show();
     });
 
-	// $("#icon_bar").click(function(){
-	// 	$("#search").hide();
-	// 	});
+    $("#mainnav button").each(function(index){
+        $(this).click(function(){
+					$("#mainnav button").removeClass("main_active");
+					$("#mainnav button").eq(index).addClass("main_active");
+        });
+    });
 });
 
